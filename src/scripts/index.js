cd6ba35896e530ccce4a9b659e10265def70267d
Casey Cook <casey.cook.eher@statefarm.com>
2021-07-13 09:38:55 -0500

    get the promise working

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+document.getElementById("generate-idea").addEventListener('click', function(){
+    Promise.all([(fetch("http://www.boredapi.com/api/activity/?price=0.0").then(response => response.json()))])
+        .then(response => console.log(response))
+        
+    
+})
