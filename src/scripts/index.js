1f7dec0bac4f9836f3252b8b7b720ce7cd5e8758
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-13 07:58:02 -0700

    add json payload data to generateidea

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,16 @@
 document.getElementById("generate-idea").addEventListener('click', function(){
+    let output = document.getElementById("suggestion-output");
+
     Promise.all([(fetch("http://www.boredapi.com/api/activity/?price=0.0").then(response => response.json()))])
-        .then(response => console.log(response));
+        .then(response => {
+            // activity
+            console.log(response[0]);
+            console.log(response[0].activity);
+            // type
+            console.log(response[0].type);
+            // participants
+            console.log(response[0].participants);
+ 
+        });
 
 })
