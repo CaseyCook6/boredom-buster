ed912e550f8b415d07059913bba54098dcc93b39
tod-easton <tod.easton.fv6o@statefarm.com>
2021-07-13 10:27:37 -0700

    add validation logic for contactUs page

--- src/scripts/contactUs.js
+++ src/scripts/contactUs.js
@@ -1,7 +1,30 @@
+function validateForm() {
+  let name = document.querySelector("#contact-us-form #name").value;
+  let comment = document.querySelector("#contact-us-form #comment").value;
+  
+  if (name === "" && comment === "") {
+    alert("Name and Comment must be filled out.");
+    return false;
+  } else if (name === "") {
+    alert("Name must be filled out.");
+    return false;
+  } else if (comment === "") {
+    alert("Comment must be filled out.");
+    return false;
+  } else if (comment.length <= 3) {
+    alert("Comment must be more than 3 characters.");
+    return false;
+  }
+  return true;
+}
+
+
 document.getElementById("submit-comment-button").addEventListener('click', function(){
+  let finish = validateForm();
     let submitterName = document.querySelector("#contact-us-form #name").value;
     let thankYou = document.querySelector("#thank-you p");
-  
-    thankYou.innerText = `Thank you ${submitterName} for your comment!`;
-    
+
+    if (finish) {
+      thankYou.innerText = `Thank you ${submitterName} for your comment!`;
+    }   
   });
