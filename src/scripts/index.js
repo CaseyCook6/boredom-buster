a82ffaf48b5d23210b11112494c2f38660bbb05e
Christopher Mochel <christopher.mochel.eu84@statefarm.com>
2021-07-13 10:59:59 -0500

    refactor querySelectors to use new  html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,13 @@
 document.getElementById("generate-idea").addEventListener('click', function(){
-    let output = document.querySelector("#suggestion-output > p");
-    // output.innerText = " ";
+    let outputActivity = document.querySelector("#suggestion-output .activity span");
+    let outputParticipants = document.querySelector("#suggestion-output .participants span");
+    let outputActivityType = document.querySelector("#suggestion-output .activity-type span");
 
+    //use promise.all to make mutli-call expansion easier
     Promise.all([(fetch("http://www.boredapi.com/api/activity/?price=0.0").then(response => response.json()))])
         .then(response => {
-            output.innerText = response[0].activity + response[0].type + response[0].participants; 
+            outputActivity.innerText = response[0].activity;
+            outputParticipants.innerText = response[0].participants;
+            outputActivityType.innerText = response[0].type;
         });
 })
