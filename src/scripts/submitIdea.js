cf234943dbeef51da650fcde87a1b9f47fefe01b
Casey Cook <casey.cook.eher@statefarm.com>
2021-07-13 12:10:06 -0500

    update validateForm function to check for name, idea, and idea length

--- src/scripts/submitIdea.js
+++ src/scripts/submitIdea.js
@@ -1,18 +1,29 @@
 function validateForm() {
   let name = document.querySelector("#submit-a-form #name").value;
-  if (name == "") {
-    alert("Name must be filled out");
+  let idea = document.querySelector("#submit-a-form #idea").value;
+  
+  if (name === "" && idea === "") {
+    alert("Name and Idea must be filled out.");
+    return false;
+  } else if (name === "") {
+    alert("Name must be filled out.");
+    return false;
+  } else if (idea === "") {
+    alert("Idea must be filled out.");
+    return false;
+  } else if (idea.length <= 3) {
+    alert("Idea must be more than 3 characters.");
     return false;
   }
   return true;
 }
 
 document.getElementById("idea-button").addEventListener('click', function(){
+  let ideaConfirmation = document.querySelector("#idea-confirmation p");
   let finish = validateForm();
+
   if (finish) {
     let submitterName = document.querySelector("#submit-a-form #name").value;
-    let ideaConfirmation = document.querySelector("#idea-confirmation p");
-
     ideaConfirmation.innerText = `Thank you ${submitterName} for your idea!`;
   } 
 });
