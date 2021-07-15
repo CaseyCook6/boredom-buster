5c1b64f36f8d0a78d91669af0552ca5a918641d9
Casey Cook <casey.cook.eher@statefarm.com>
2021-07-15 16:12:53 -0500

    update link to https for fetch

--- src/scripts/generateIdea.js
+++ src/scripts/generateIdea.js
@@ -4,7 +4,7 @@ document.getElementById("generate-idea").addEventListener('click', function(){
     let outputActivityType = document.querySelector("#suggestion-output .activity-type span");
 
     //use promise.all to make mutli-call expansion easier
-    Promise.all([(fetch("http://www.boredapi.com/api/activity/?price=0.0").then(response => response.json()))])
+    Promise.all([(fetch("https://www.boredapi.com/api/activity/?price=0.0").then(response => response.json()))])
         .then(response => {
             outputActivity.innerText = response[0].activity;
             outputParticipants.innerText = response[0].participants;
