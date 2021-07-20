77f387ebac4473622d2be111cd419013d8e93f50
ChaoticNeutralCode <rich@busyrich.com>
2021-07-20 14:36:01 -0700

    Generic form validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,24 @@
 const currentDate = new Date();
 document.querySelector("footer p").innerText = "\251"+` Boredom Busters, LLC ${currentDate.getFullYear()}`;
+
+// joins with commas but adds "and" for the last element
+// https://stackoverflow.com/a/49289042/984078
+function prettyJoin(arr) {
+  return arr
+    .join(', ')
+    .replace(/, ([^,]*)$/, `${arr.length > 2 ? ',': ''} and $1`);
+}
+
+function validateForm(form) {
+  let invalids = [];
+
+  for(let elm of form.querySelectorAll('input, textarea')) {
+    if(!elm.value) invalids.push(elm.name);
+  }
+
+  if(invalids.length) {
+    return `The following fields are required: ${prettyJoin(invalids)}`;
+  }
+
+  return '';
+}
--- src/scripts/submitIdea.js
+++ src/scripts/submitIdea.js
@@ -1,30 +1,12 @@
-function validateForm() {
-  let name = document.querySelector("#submit-a-form #name").value;
-  let idea = document.querySelector("#submit-a-form #idea").value;
-  
-  if (name === "" && idea === "") {
-    alert("Name and Idea must be filled out.");
-    return false;
-  } else if (name === "") {
-    alert("Name must be filled out.");
-    return false;
-  } else if (idea === "") {
-    alert("Idea must be filled out.");
-    return false;
-  } else if (idea.length <= 3) {
-    alert("Idea must be more than 3 characters.");
-    return false;
-  }
-  return true;
-}
-
 document.getElementById("idea-button").addEventListener('click', function(){
   let ideaConfirmation = document.querySelector("#idea-confirmation");
-  let finish = validateForm();
+  let error = validateForm(document.querySelector('#submit-a-form form'));
 
-  if (finish) {
+  if (!error) {
     let submitterName = document.querySelector("#submit-a-form #name").value;
     ideaConfirmation.querySelector('.submitterName').innerText = submitterName;
     ideaConfirmation.classList.remove('hide');
-  } 
+  } else {
+    alert(error);
+  }
 });
